5ad70d6b9e684f0e730e0bd3f6ecdcde58899a8b
KananKarim <kenankerimli7770@gmail.com>
2024-04-03 14:22:20 +0400

    tests added

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Test for DB storage """
+from os import getenv
+import unittest
+from models.base_model import BaseModel
+from models import storage
+
+
+@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") != "db", "DBStorage")
+class TestDBStorage(unittest.TestCase):
+    """ Test DBStorage class """
+
+    def test_all(self):
+        """ test all method """
+        new = BaseModel()
+        temp = storage.all()
+        self.assertIsInstance(temp, dict)
