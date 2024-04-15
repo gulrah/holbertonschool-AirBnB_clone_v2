30266d47e8d42e90f8598c672fe1f2e0a5993b87
gulrah <gulnarrahimli@mail.ru>
2024-04-15 10:52:17 +0000

    Route Python ”, followed by the value of the text variabledisplays

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,33 +1,53 @@
 #!/usr/bin/python3
-"""Start a Flask web app"""
-from flask import Flask, escape
+"""
+Start a Flask web app
+"""
+
+from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Route displays 'Hello HBNB!'"""
+    """
+    Route displays 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
-    """Route displays 'HBNB'"""
+    """
+    Route displays 'HBNB'
+    """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """Route that displays 'C ', followed by the value of the text variable"""
+def display_text(text):
+    """
+    Route displays 'C ' followed by the value of the text variable
+    """
     return "C {}".format(escape(text).replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
-    """Route displays 'Python ', followed by the value of the text variable"""
+    """
+    Route displays 'Python ' followed by the value of the text variable
+    """
     return "Python {}".format(escape(text).replace('_', ' '))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Handler for 404 errors
+    """
+    return "Not found", 404
 
 
 if __name__ == "__main__":
