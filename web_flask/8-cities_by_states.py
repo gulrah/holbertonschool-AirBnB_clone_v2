3e2a5dd200cd39252abdf1ae6feb51cf8b612876
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-04-20 10:01:04 -0700

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-''' Script that starts a Flask web application '''
-from flask import Flask, render_template
+"""Starts a Flask web application
+"""
 from models import storage
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def show_cities_by_states():
-    ''' Returns an HTML page showing states and its cities '''
-    obj = storage.all('State')
-    states_list = []
-    for k, v in obj.items():
-        states_list.append(v)
-    return render_template('8-cities_by_states.html', states_list=states_list)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def remove_session(exception):
-    ''' Closes the current storage session '''
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
