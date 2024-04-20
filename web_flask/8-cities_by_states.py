ab93b839c032d828b926bdcf7a83f11d635d5536
gulrah <gulnarrahimli@mail.ru>
2024-04-20 13:23:18 +0000

    little edits

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -20,13 +20,13 @@ def cities_by_states():
         states = sorted(states, key=lambda state: state.name.lower())
         
         return render_template('8-cities_by_states.html', states=states)
-    
+                            
     
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy session"""
-    storage.close()
-    
+"""Remove the current SQLAlchemy session"""
+storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
