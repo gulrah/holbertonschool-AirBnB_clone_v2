4a5877c30c2ec563da68569a2b7d35f4350d39a8
gulrah <gulnarrahimli@mail.ru>
2024-04-20 13:18:44 +0000

    Cities by States

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -25,7 +25,7 @@ def teardown(exception):
     Remove the current SQLAlchemy Session
     """
     storage.close()
-    
-    
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from os import getenv
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with states and their cities"""
+    states = storage.all(State).values()
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        states = sorted(states, key=lambda state: state.name)
+    else:
+        states = sorted(states, key=lambda state: state.name.lower())
+        
+        return render_template('8-cities_by_states.html', states=states)
+    
+    
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
